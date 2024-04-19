0b7f92deefa91e6b204c856595e0c341530218b2
Jesper Annebäck <jesper.anneback@gmail.com>
2024-04-19 10:08:00 +0200

    fix testing library matches

--- jest.config.ts
+++ jest.config.ts
@@ -15,6 +15,8 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
+  // to get testing-library matchers to work e.g. toHaveTextContent, toBeInDocument
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
