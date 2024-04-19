faefd60b868f0f8ebf9ad6af4d3c9e328f82d4d2
Jesper Annebäck <jesper.anneback@gmail.com>
2024-04-19 10:08:46 +0200

    adds Door test

--- components/__tests__/Door.test.tsx
+++ components/__tests__/Door.test.tsx
@@ -1,10 +1,31 @@
 import { render } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
 import Door from '../Door';
 
 describe('Door', () => {
-  it('should render with default label and h3', () => {
-    const actual = render(<Door id={1} />);
+  it('should render with default label, bg-color and heading', () => {
+    render(<Door id={1} />);
 
-    expect(actual).toBe(null);
+    expect(screen.getByRole('heading').textContent).toBe('Door 2');
+    expect(screen.getByRole('button').textContent).toBe('Door');
+    expect(screen.getByRole('button').getAttribute('class')).toMatch(
+      /bg-orange-600 hover:bg-orange-500 text-white/
+    );
+  });
+
+  it('should render correct bg-color for nothingDoor', () => {
+    render(<Door id={1} label='nothing' />);
+
+    expect(screen.getByRole('button').getAttribute('class')).toMatch(
+      /bg-slate-600 hover:bg-slate-500/
+    );
+  });
+
+  it('should render correct text-color for guessedDoor', () => {
+    render(<Door id={1} label='guessed' />);
+
+    expect(screen.getByRole('button').getAttribute('class')).toMatch(
+      /text-slate-600/
+    );
   });
 });
